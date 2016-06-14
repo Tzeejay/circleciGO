3d1f32cc9e716d38811f079e2a628b2a91ffc380
Constantin Jacob <c.jacob@live.de>
2016-06-14 13:34:04 -0700

    Tests running

--- circleci_test.go
+++ circleci_test.go
@@ -1,34 +1,53 @@
 package main
 
-
 import (
-  "testing"
-  "net/http"
-  "net/http/httptest"
-  "log"
-  "strings"
-  "fmt"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
-
-func TestAppIO (t *testing.T) {
-  handler := func(w http.ResponseWriter, r *http.Request) {
+func TestWithNoText(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "something failed", http.StatusInternalServerError)
 	}
 
-  req, err := http.NewRequest("GET", "http://google.com", nil)
-  	if err != nil {
-  		log.Fatal(err)
-  	}
+	req, err := http.NewRequest("GET", "http://cjcircle.herokuapp.com/", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  w := httptest.NewRecorder()
+	w := httptest.NewRecorder()
 	handler(w, req)
 
-string := w.Body.String()
-subString := "Yay new text is showing :)"
-fmt.Printf("%s",  string)
-if !strings.Contains(string, subString) {
-  t.Fail()
+	string := w.Body.String()
+	subString := "Yay new text is showing :)"
+	fmt.Printf("%s", string)
+	if strings.Contains(string, subString) {
+		t.Fail()
+	}
 }
+
+func TestWithText(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "something failed", http.StatusInternalServerError)
+	}
+
+	req, err := http.NewRequest("GET", "http://cjcircle.herokuapp.com/iwasclicked", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	string := w.Body.String()
+	subString := "Yay new text is showing :)"
+	fmt.Printf("%s", string)
+	if strings.Contains(string, subString) {
+		t.Fail()
+	}
 
 }
